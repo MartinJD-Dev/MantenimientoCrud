f5f8ad52edceabeecd93cbae2de04582f8ff9274
MartinJD-Dev <me0022jd@gmail.com>
2025-02-27 16:52:58 -0600

    Listado General para ver un top de 10 y con paginacion

---
+++ MantenimientoCrud/Controllers/MantenimientoController.cs
@@ -0,0 +1,41 @@
+﻿using MantenimientoCrud.Models;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MantenimientoCrud.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        string connectionString;
+        DB_AR_AccountDataContext db;
+
+        public MantenimientoController()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["DB_9E3FAF_CIFASAConnectionString"].ConnectionString;
+            db = new DB_AR_AccountDataContext(connection: connectionString);
+        }
+
+        [HttpGet]
+        public ActionResult Inicio(int pageNumber = 1, int pageSize = 10)
+        {
+            using (var context = new DB_AR_AccountDataContext(connection: connectionString))
+            {
+                int totalaccount = context.sp_ObtenerCuenta().Count();
+
+                int totalPages = (int)Math.Ceiling((double)totalaccount / pageSize);
+
+                var productos = context.sp_ObtenerCuenta().
+                                        OrderBy(c => c.ID).
+                                        Skip((pageNumber - 1) * pageSize).
+                                        Take(pageSize).ToList();
+
+                var model = new PaginatedLists<sp_ObtenerCuentaResult>(productos, totalaccount, pageNumber, pageSize);
+
+                return View(model);
+            }
+        }
+
+    }
+}
---
+++ MantenimientoCrud/Models/PaginatedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManteminientoCrud.Models
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public PaginatedList(IEnumerable<T> items, int totalCount, int pageNumber, int pageSize)
+        {
+            this.AddRange(items);
+            TotalCount = totalCount;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(TotalCount / (double)PageSize);
+        }
+
+        public int TotalCount { get; }
+        public int TotalPages { get; }
+        public int PageNumber { get; }
+        public int PageSize { get; }
+
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+    }
+}
