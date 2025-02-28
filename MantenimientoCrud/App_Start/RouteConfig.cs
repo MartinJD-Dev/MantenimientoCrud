06aa2a05d27dfb60be0b8057af55d03dca9a65f2
MartinJD-Dev <me0022jd@gmail.com>
2025-02-28 00:34:28 -0600

    Un login de fantasia que cuando le den ingresar, permita mostrar el menu con el mantenimiento que vayan a realizar.

--- MantenimientoCrud/App_Start/RouteConfig.cs
+++ MantenimientoCrud/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MantenimientoCrud
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Mantenimiento", action = "Inicio", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
